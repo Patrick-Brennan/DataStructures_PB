4704551f72380edef71589cca5f6b455d599c76a
Patrick-Brennan <patrickb2@hotmail.ca>
2021-02-03 17:57:56 -0500

    Working on first excercise.

---
+++ src/Arrays/App.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+        int[] intArray = new int[4];
+        int x = intArray.length;
+
+        intArray[0] = 1;
+
+        int size;
+        System.out.println("Choose the size you want. (Between 1 and 100)");
+        System.console() = size;
+
+        if (size < 1 | size > 100) {
+            System.out.println("The size has to be within 1 and 100.");
+            return;
+        }
+
+
+        System.out.println(sizeArray[1]);
+    }
+}
